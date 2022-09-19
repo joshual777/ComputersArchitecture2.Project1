041eccba6c89ae36164ae33a1f5ca542b0f0d74e
Joshua <josui77@hotmail.com>
2022-09-19 16:01:04 -0600

    Cache L1 Parameters Info
    The four parameters in cache to manage their information

---
+++ Dev/Simulador MESI/Cache_Info_Parameters.py
@@ -0,0 +1,56 @@
+"""
+								CACHE MEMORY MODEL AND SYSTEM COHERENCE
+
+		Following the project design specification, each block requires specific information
+		they can store 4 memoty blocks and de the information is:
+
+			-->  Number of bock
+			-->  Coherence state by the project: M = modified, S = shared, I= invalid, E = exclusive
+			-->  Memory address
+			-->  Hexadecimal 16 lenght data
+"""
+#CLASS TO MANAGE THE INFO ABOUT THE CACHE AND ITS PARAMETERS 
+class L1_Info:
+
+	#The atributes are initialized
+	def  __init__(self, Number):
+		self.Number = Number   		#Number of block
+		self.bitState = "I"			#State of the block according to the MESI Protocol
+		self.memoryAddress = 0		#Memory address in the process
+		self.data = 0				#Hexadecimal data in the process
+
+
+#This function will get the number block and set the value of the block
+def getNumber(self):
+        	return self.Number
+def setNumber(self, Number):
+        	self.Number = Number
+
+
+#This function will get the state block and set the state of the block according to the MESI protocol
+def getBitState(self):
+        	return self.bitState
+
+def setBitState(self, state):
+        	self.bitState = state
+
+
+##This function will get the address block and set the address of the block
+def getAddress(self):
+        	return self.memoryAddress
+
+def setAddress(self, address):
+        	self.memoryAddress = address
+
+
+#This function will get the hexadecimal data block and set the hexadecimal data of the block
+def getData(self):
+        	return self.data
+
+def setData(self, data):
+        	self.data = data
+
+
+#Monitory function just in case, this will help to the GUI
+def printBlock(self):
+        	print("ID " + str(self.Number) + "  State " + str(self.bitState)+ "  Address " + str(self.memoryAddress) +"  Data " + str(self.data)  )
