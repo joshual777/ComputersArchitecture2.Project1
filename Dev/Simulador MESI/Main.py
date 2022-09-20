dec0184850038523fc5eb5f611f5f0bc37c0a1c1
Joshua <josui77@hotmail.com>
2022-09-20 09:13:47 -0600

    GUI items arrange and deploy as a main function

---
+++ Dev/Simulador MESI/Main.py
@@ -0,0 +1,12 @@
+from SharedBus import MemoryBus
+from Memory import Memory
+from Processor import Processor
+from L1_Cache_Module import L1_Block
+import time
+from GUI import interfaz
+from tkinter import *
+
+if __name__ == "__main__":
+	root = Tk()
+	ui = interfaz(root)
+	root.mainloop()
